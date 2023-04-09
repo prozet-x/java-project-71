a77755865c87b9e90fec1d732b72c1827e9a492b
prozet <abcd-xyz@list.ru>
2023-04-10 00:05:22 +0700

    st 6

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DifferTest {
+    @Test
+    public void testS() {
+        
+    }
+}
