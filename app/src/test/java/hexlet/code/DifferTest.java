da58fd257f7006de433cc40597a0290022b5b9ee
prozet <abcd-xyz@list.ru>
2023-04-10 23:56:25 +0700

    st 6

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -13,26 +13,37 @@ import java.util.Map;
 
 class DifferTest {
     @Test
-    public void testS() throws Exception {
-        var fix1Path = "../../resourses/fix1.json";
-        var fix2Path = "../../resourses/fix2.json";
+    public void testS() {
+        var fix1Path = "src/test/resources/fix1.json";
+        var fix2Path = "src/test/resources/fix2.json";
+        var fix1fix2diffPath = "src/test/resources/f1f2res.txt";
+        var fix2fix1diffPath = "src/test/resources/f2f1res.txt";
+
         String file1Data;
         String file2Data;
+        String f1f2diffData;
+        String f2f1diffData;
         try {
             file1Data = Files.readString(Path.of(fix1Path));
             file2Data = Files.readString(Path.of(fix2Path));
+            f1f2diffData = Files.readString(Path.of(fix1fix2diffPath));
+            f2f1diffData = Files.readString(Path.of(fix2fix1diffPath));
         } catch (IOException ex) {
             System.out.println("File " + ex.getMessage() + " does not exist.");
-            return;
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
             return;
         }
 
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, ?> json1 = mapper.readValue(file1Data, new TypeReference<Map<String, ?>>() { });
-        Map<String, ?> json2 = mapper.readValue(file2Data, new TypeReference<Map<String, ?>>() { });
-        System.out.println(Differ.generate(json1, json2));
-
+        Map<String, ?> json1;
+        Map<String, ?> json2;
+        try {
+            json1 = mapper.readValue(file1Data, new TypeReference<Map<String, ?>>() {});
+            json2 = mapper.readValue(file2Data, new TypeReference<Map<String, ?>>() {});
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+        assertEquals(f1f2diffData, Differ.generate(json1, json2));
+        assertEquals(f2f1diffData, Differ.generate(json2, json1));
     }
 }
